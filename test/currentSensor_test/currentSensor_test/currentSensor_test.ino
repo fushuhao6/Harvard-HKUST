1f96c31e99929916870b66bd068ccb3d1ac94d46
camakk <camakk@gmail.com>
2016-07-26 12:01:28 -0400

    added more troubleshoot lines to current sensor code
    print raw and adjusted values

--- test/currentSensor_test/currentSensor_test/currentSensor_test.ino
+++ test/currentSensor_test/currentSensor_test/currentSensor_test.ino
@@ -1,7 +1,7 @@
 // currentSensor_test.ino
 // working as of 25 July 2016
 
-const uint8_t currentSensorPin = A8;
+const uint8_t currentSensorPin = A0;
 double currentSensorRaw = 0;
 double current = 0;
 
@@ -12,25 +12,28 @@ void setup() {
 
 void loop() {
   analogReadResolution(12);
-  currentSensorRaw = (double) analogRead(currentSensorPin);
+  currentSensorRaw = (double) analogRead(A0);
   
   // VOLTAGE DIVIDER TAKES 0.66 of the output voltage from motor controller (between 0 and 5)
   // in order to center it around 1.65V. Now is 43.56mV/A or 54.0408 ~54 per amp
   // 0        2047      4095    value in currentSensorRaw
   // 0V      1.65V     3.3V     corresponds to these voltages raw, after divider
+
+  // 1.36 V is the actual 0V
   
-  current = currentSensorRaw - 2047;
+  current = currentSensorRaw - 1600;
   
   // Now centered around 0 so we have
   //  -2047       0       2047
   // -37.88A      0      37.88A
   // need to divide raw value by 54.08 to get amperage
-
-  current = current / 54.08; // this is a constant value we need to get actual current
+  current = current / 174.08; // this is a constant value we need to get actual current
   
   Serial.print("\nCurrent: ");  
   Serial.print(current, 3);
-  Serial.print("A");
+  Serial.print(" A      ");
+  Serial.print("Raw Value: ");
+  Serial.print(currentSensorRaw, 4);
   
-  delay(100);
+  delay(1000);
 }
