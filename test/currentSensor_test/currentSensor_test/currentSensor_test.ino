380d3bc0b2ce932263ae63c6aefffb43a46fcedd
camakk <camakk@gmail.com>
2016-07-26 15:54:33 -0400

    semi-functional motor code right before cameron left
    try inverting around the pivot value of 2700

--- test/currentSensor_test/currentSensor_test/currentSensor_test.ino
+++ test/currentSensor_test/currentSensor_test/currentSensor_test.ino
@@ -14,26 +14,17 @@ void loop() {
   analogReadResolution(12);
   currentSensorRaw = (double) analogRead(A0);
   
-  // VOLTAGE DIVIDER TAKES 0.66 of the output voltage from motor controller (between 0 and 5)
-  // in order to center it around 1.65V. Now is 43.56mV/A or 54.0408 ~54 per amp
-  // 0        2047      4095    value in currentSensorRaw
-  // 0V      1.65V     3.3V     corresponds to these voltages raw, after divider
-
-  // 1.36 V is the actual 0V
+  // New circuit is just 1k resistor series a 3.3V Zener Diode
+  // the zero current value is 875
+  // need to divide result by 200 to get actual current
   
-  current = currentSensorRaw - 1600;
-  
-  // Now centered around 0 so we have
-  //  -2047       0       2047
-  // -37.88A      0      37.88A
-  // need to divide raw value by 54.08 to get amperage
-  current = current / 174.08; // this is a constant value we need to get actual current
+  current = (currentSensorRaw - 2425)/250;
   
   Serial.print("\nCurrent: ");  
-  Serial.print(current, 3);
+  Serial.print(current, 2);
   Serial.print(" A      ");
   Serial.print("Raw Value: ");
   Serial.print(currentSensorRaw, 4);
   
-  delay(1000);
+  delay(100);
 }
